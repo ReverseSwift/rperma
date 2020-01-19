7ecd5ff463a75eca86bd91899b82296200513861
SwiftReverse <moomace12@gmail.com>
2020-01-19 20:50:03 +0000

    Update config.lua

--- reverse_perma_weapons/lua/reverseperma/config.lua
+++ reverse_perma_weapons/lua/reverseperma/config.lua
@@ -1,3 +1,8 @@
+-[[
+Commissioned By: Reverse
+Produced By: Tasid
+--]]
+
 rperma.config = {}
 
 rperma.config.durabilityloss = 1
@@ -14,68 +19,9 @@ rperma.config.categories = {
 }
 
 rperma.config.weapons = {
-	["tfa_starwars_incendiary"] = {
+	["weapon_ak472"] = {
 		price = 250000,
 		category = "Knives",
 	},
-	["tfa_starwars_shock"] = {
-		price = 250000,
-		category = "Knives",
-	},
-	["weapon_752_bowcaster"] = {
-		price = 250000,
-		category = "Knives",
-	},
-	["weapon_752_dh17"] = {
-		price = 250000,
-		category = "Knives",
-	},	
-	["weapon_752_kotor_blaster_rifle"] = {
-		price = 250000,
-		category = "Knives",
-	},
-	["weapon_752_kyd21"] = {
-		price = 250000,
-		category = "Knives",
-	},
-	["tfa_kotor_br_2"] = {
-		price = 250000,
-		category = "Knives",
-	},
-	["tfa_swch_dc15a"] = {
-		price = 250000,
-		category = "Knives",
-	},	
-	["tfa_swch_e5"] = {
-		price = 250000,
-		category = "Guns",
-	},
-	["tfa_sw_cisshot"] = {
-		price = 250000,
-		category = "Guns",
-	},
-	["tfa_swch_de10"] = {
-		price = 250000,
-		category = "Guns",
-	},
-	["tfa_sw_dual_kyd21"] = {
-		price = 250000,
-		category = "Guns",
-	},	
-	["tfa_752_dlt19"] = {
-		price = 250000,
-		category = "Guns",
-	},
-	["tfa_dh17"] = {
-		price = 250000,
-		category = "Guns",
-	},
-	["tfa_dc17m_shotgun"] = {
-		price = 250000,
-		category = "Guns",
-	},
-	["tfa_swch_dc17m_br"] = {
-		price = 250000,
-		category = "Guns",
-	},	
+	
 }
